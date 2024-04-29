296af5575121b03d7a6f91a15ced83db1df906f4
Luis Bustamante <luisebt995@gmail.com>
2024-04-29 17:31:45 +0200

    Initial Commit

---
+++ PruebaFinalModular/PruebaFinalModularApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaFinalModularApp.swift
+//  PruebaFinalModular
+//
+//  Created by Luis on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaFinalModularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
